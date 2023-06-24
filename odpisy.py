e07dc2797fd23ca85017092a1b152b2273e0249d
slany3.jan <97552526+slanja@users.noreply.github.com>
2023-06-24 20:44:44 +0200

    improved using functions

--- odpisy.py
+++ odpisy.py
@@ -1,143 +1,122 @@
 import math
 from prettytable import PrettyTable
 
-while True:
-	######## Inputy ##########
-	try:
-		vstupni_cena = int(input("Zadej vstupní cenu: "))
-		odpisova_skupina = input("Zadej odpisovou skupinu: ")
-		typ_odpisu = input("[R]Rovnoměrný / [Z]Zrychlený: ").lower()
-		print()
-	except ValueError as e:
-		print("Když to musí bejt")
-	except Exception as e:
-		print(e)
+# straight-line depreciation
+def straight_line(odpisova_skupina, vstupni_cena):
+    match odpisova_skupina:
+        case "1":
+            odpis_prvni_rok = 20
+            dalsi_roky = 40
+            doba_odpisovani = 3
+        case "2":
+            odpis_prvni_rok = 11
+            dalsi_roky = 22.25
+            doba_odpisovani = 5
+        case "3":
+            odpis_prvni_rok = 5.5
+            dalsi_roky = 10.5
+            doba_odpisovani = 10
+        case "4":
+            odpis_prvni_rok = 2.15
+            dalsi_roky = 5.15
+            doba_odpisovani = 20
+        case "5":
+            odpis_prvni_rok = 1.4
+            dalsi_roky = 3.4
+            doba_odpisovani = 30
+        case "6":
+            odpis_prvni_rok = 1.02
+            dalsi_roky = 2.02
+            doba_odpisovani = 50
 
-	######## Odpisová skupina - Rovnoměrná ##########
-	try:
-		if typ_odpisu.startswith("r"):
-			match odpisova_skupina:
-				case "1":
-					odpis_prvni_rok = 20
-					dalsi_roky = 40
-					doba_odpisovani = 3
-				case "2":
-					odpis_prvni_rok = 11
-					dalsi_roky = 22.25
-					doba_odpisovani = 5
-				case "3":
-					odpis_prvni_rok = 5.5
-					dalsi_roky = 10.5
-					doba_odpisovani = 10
-				case "4":
-					odpis_prvni_rok = 2.15
-					dalsi_roky = 5.15
-					doba_odpisovani = 20
-				case "5":
-					odpis_prvni_rok = 1.4
-					dalsi_roky = 3.4
-					doba_odpisovani = 30
-				case "6":
-					odpis_prvni_rok = 1.02
-					dalsi_roky = 2.02
-					doba_odpisovani = 50
+    zustatek = vstupni_cena
+    opravky = 0 
 
-		######## Odpisová skupina - Zrychlená ##########
-		elif typ_odpisu.startswith("z"):
+    table = PrettyTable(["Odpis", "Opravky", "Zůstatek"])
+                
+    # mathematical operations
+    for rok in range(doba_odpisovani):
+        if rok == 0:
+            odpis = math.ceil(vstupni_cena*odpis_prvni_rok*0.01)
+        else:
+            odpis = math.ceil(vstupni_cena*dalsi_roky*0.01)
 
-			match odpisova_skupina:
-				case "1":
-					odpis_prvni_rok = 3
-					dalsi_roky = 4
-					doba_odpisovani = 3
-				case "2":
-					odpis_prvni_rok = 5
-					dalsi_roky = 6
-					doba_odpisovani = 5
-				case "3":
-					odpis_prvni_rok = 10
-					dalsi_roky = 11
-					doba_odpisovani = 10
-				case "4":
-					odpis_prvni_rok = 20
-					dalsi_roky = 21
-					doba_odpisovani = 20
-				case "5":	
-					odpis_prvni_rok = 30
-					dalsi_roky = 31
-					doba_odpisovani = 30
-				case "6":
-					odpis_prvni_rok = 50
-					dalsi_roky = 51
-					doba_odpisovani = 50
+        opravky += math.ceil(odpis)
+        zustatek -= odpis
+                        
+        table.add_row([odpis, opravky, zustatek])
 
-	except NameError as e:
-		print("Takhle to nepůjde!")
-	except Exception as e:
-		print(e)
+    table.add_autoindex("Rok")
+    print(table)
 
-	######## Rovnoměrné ##########
-	try:
-		if typ_odpisu.startswith("r"):
-			try:
-				zustatek = vstupni_cena
-				opravky = 0 
+# accelerated depreciation
+def accelerated(odpisova_skupina, vstupni_cena):
+    match odpisova_skupina:
+        case "1":
+            odpis_prvni_rok = 3
+            dalsi_roky = 4
+            doba_odpisovani = 3
+        case "2":
+            odpis_prvni_rok = 5
+            dalsi_roky = 6
+            doba_odpisovani = 5
+        case "3":
+            odpis_prvni_rok = 10
+            dalsi_roky = 11
+            doba_odpisovani = 10
+        case "4":
+            odpis_prvni_rok = 20
+            dalsi_roky = 21
+            doba_odpisovani = 20
+        case "5":   
+            odpis_prvni_rok = 30
+            dalsi_roky = 31
+            doba_odpisovani = 30
+        case "6":
+            odpis_prvni_rok = 50
+            dalsi_roky = 51
+            doba_odpisovani = 50
 
-				table = PrettyTable(["Odpis", "Opravky", "Zůstatek"])
-				
-				for rok in range(doba_odpisovani):
-					if rok == 0:
-						odpis = math.ceil(vstupni_cena*odpis_prvni_rok*0.01)
-					else:
-						odpis = math.ceil(vstupni_cena*dalsi_roky*0.01)
+    zustatek = vstupni_cena
+    opravky = 0
 
-					opravky += math.ceil(odpis)
-					zustatek -= odpis
-						
-					table.add_row([odpis, opravky, zustatek])
-				table.add_autoindex("Rok")
-				print(table)
+    table = PrettyTable(["Odpis", "Opravky", "Zůstatek"])
 
-			except NameError as e:
-				print("Zadej číslo né písmeno")
-			except Exception as e:
-				print(e)
+    # mathematical operations
+    for rok in range(doba_odpisovani):
+        if rok == 0:
+            odpis = math.ceil(zustatek / odpis_prvni_rok)
+        else:
+            odpis = math.ceil((2 * zustatek) / (dalsi_roky - rok))         
 
-		######## Zrychelené ##########
-		elif typ_odpisu.startswith("z"): 
-			try:
+        opravky += math.ceil(odpis)  
+        zustatek -= odpis
 
-				zustatek = vstupni_cena
-				opravky = 0
+        table.add_row([odpis, opravky, zustatek])
+    table.add_autoindex("Rok")
+    print(table)
+    
 
-				table = PrettyTable(["Odpis", "Opravky", "Zůstatek"])
+def main():
+    # inputs
+    vstupni_cena = int(input("Zadej vstupní cenu: "))
+    odpisova_skupina = input("Zadej odpisovou skupinu: ")
+    typ_odpisu = input("[R]Rovnoměrný / [Z]Zrychlený: ").lower()
+    print()
 
-				for rok in range(doba_odpisovani):
-					if rok == 0:
-						odpis = math.ceil(zustatek / odpis_prvni_rok)
-					else:
-						odpis = math.ceil((2 * zustatek) / (dalsi_roky - rok))	       
+    if typ_odpisu.lower() == "r":
+        straight_line(odpisova_skupina, vstupni_cena)
+    else:
+        accelerated(odpisova_skupina, vstupni_cena)
 
-					opravky += math.ceil(odpis)  
-					zustatek -= odpis
+    # repeating cycle
+    again = input("Chceš počítat dále? ano/ne ").lower()
 
-					table.add_row([odpis, opravky, zustatek])
-				table.add_autoindex("Rok")
-				print(table)
+    if again != "ano":
+        pass
+    else:
+        main()
 
-			except NameError as e:
-				print("Zadej číslo né písmeno")
-			except Exception as e:
-				print(e)
 
-	except NameError as e:
-		print("Vážně")
-	except Exception as e:
-		print(e)
-
-	again = input("Chceš počítat dále? ano/ne ").lower()
-
-	if again != "ano":
-		break
-	else:
-		pass
+if __name__ == "__main__":
+    main()
