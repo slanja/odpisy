ec947cfa078afaa1adac9e178f4bf9a6eb9fb05d
slany3.jan <97552526+slanja@users.noreply.github.com>
2023-06-24 20:58:31 +0200

    englished variables

--- odpisy.py
+++ odpisy.py
@@ -2,112 +2,112 @@ import math
 from prettytable import PrettyTable
 
 # straight-line depreciation
-def straight_line(odpisova_skupina, vstupni_cena):
-    match odpisova_skupina:
+def straight_line(depreciation_group, entry_price):
+    match depreciation_group:
         case "1":
-            odpis_prvni_rok = 20
-            dalsi_roky = 40
-            doba_odpisovani = 3
+            first_year_depreciation = 20
+            other_years = 40
+            amortisation_period = 3
         case "2":
-            odpis_prvni_rok = 11
-            dalsi_roky = 22.25
-            doba_odpisovani = 5
+            first_year_depreciation = 11
+            other_years = 22.25
+            amortisation_period = 5
         case "3":
-            odpis_prvni_rok = 5.5
-            dalsi_roky = 10.5
-            doba_odpisovani = 10
+            first_year_depreciation = 5.5
+            other_years = 10.5
+            amortisation_period = 10
         case "4":
-            odpis_prvni_rok = 2.15
-            dalsi_roky = 5.15
-            doba_odpisovani = 20
+            first_year_depreciation = 2.15
+            other_years = 5.15
+            amortisation_period = 20
         case "5":
-            odpis_prvni_rok = 1.4
-            dalsi_roky = 3.4
-            doba_odpisovani = 30
+            first_year_depreciation = 1.4
+            other_years = 3.4
+            amortisation_period = 30
         case "6":
-            odpis_prvni_rok = 1.02
-            dalsi_roky = 2.02
-            doba_odpisovani = 50
+            first_year_depreciation = 1.02
+            other_years = 2.02
+            amortisation_period = 50
 
-    zustatek = vstupni_cena
-    opravky = 0 
+    remainder = entry_price
+    corrections = 0 
 
     table = PrettyTable(["Odpis", "Opravky", "Zůstatek"])
                 
     # mathematical operations
-    for rok in range(doba_odpisovani):
-        if rok == 0:
-            odpis = math.ceil(vstupni_cena*odpis_prvni_rok*0.01)
+    for year in range(amortisation_period):
+        if year == 0:
+            write_off = math.ceil(entry_price * first_year_depreciation * 0.01)
         else:
-            odpis = math.ceil(vstupni_cena*dalsi_roky*0.01)
+            write_off = math.ceil(entry_price * other_years * 0.01)
 
-        opravky += math.ceil(odpis)
-        zustatek -= odpis
+        corrections += math.ceil(write_off)
+        remainder -= write_off
                         
-        table.add_row([odpis, opravky, zustatek])
+        table.add_row([write_off, corrections, remainder])
 
     table.add_autoindex("Rok")
     print(table)
 
 # accelerated depreciation
-def accelerated(odpisova_skupina, vstupni_cena):
-    match odpisova_skupina:
+def accelerated(depreciation_group, entry_price):
+    match depreciation_group:
         case "1":
-            odpis_prvni_rok = 3
-            dalsi_roky = 4
-            doba_odpisovani = 3
+            first_year_depreciation = 3
+            other_years = 4
+            amortisation_period = 3
         case "2":
-            odpis_prvni_rok = 5
-            dalsi_roky = 6
-            doba_odpisovani = 5
+            first_year_depreciation = 5
+            other_years = 6
+            amortisation_period = 5
         case "3":
-            odpis_prvni_rok = 10
-            dalsi_roky = 11
-            doba_odpisovani = 10
+            first_year_depreciation = 10
+            other_years = 11
+            amortisation_period = 10
         case "4":
-            odpis_prvni_rok = 20
-            dalsi_roky = 21
-            doba_odpisovani = 20
+            first_year_depreciation = 20
+            other_years = 21
+            amortisation_period = 20
         case "5":   
-            odpis_prvni_rok = 30
-            dalsi_roky = 31
-            doba_odpisovani = 30
+            first_year_depreciation = 30
+            other_years = 31
+            amortisation_period = 30
         case "6":
-            odpis_prvni_rok = 50
-            dalsi_roky = 51
-            doba_odpisovani = 50
+            first_year_depreciation = 50
+            other_years = 51
+            amortisation_period = 50
 
-    zustatek = vstupni_cena
-    opravky = 0
+    remainder = entry_price
+    corrections = 0
 
     table = PrettyTable(["Odpis", "Opravky", "Zůstatek"])
 
     # mathematical operations
-    for rok in range(doba_odpisovani):
-        if rok == 0:
-            odpis = math.ceil(zustatek / odpis_prvni_rok)
+    for year in range(amortisation_period):
+        if year == 0:
+            write_off = math.ceil(remainder / first_year_depreciation)
         else:
-            odpis = math.ceil((2 * zustatek) / (dalsi_roky - rok))         
+            write_off = math.ceil((2 * remainder) / (other_years - year))         
 
-        opravky += math.ceil(odpis)  
-        zustatek -= odpis
+        corrections += math.ceil(write_off)  
+        remainder -= write_off
 
-        table.add_row([odpis, opravky, zustatek])
+        table.add_row([write_off, corrections, remainder])
     table.add_autoindex("Rok")
     print(table)
-    
+
 
 def main():
     # inputs
-    vstupni_cena = int(input("Zadej vstupní cenu: "))
-    odpisova_skupina = input("Zadej odpisovou skupinu: ")
+    entry_price = int(input("Zadej vstupní cenu: "))
+    depreciation_group = input("Zadej odpisovou skupinu: ")
     typ_odpisu = input("[R]Rovnoměrný / [Z]Zrychlený: ").lower()
     print()
 
     if typ_odpisu.lower() == "r":
-        straight_line(odpisova_skupina, vstupni_cena)
+        straight_line(depreciation_group, entry_price)
     else:
-        accelerated(odpisova_skupina, vstupni_cena)
+        accelerated(depreciation_group, entry_price)
 
     # repeating cycle
     again = input("Chceš počítat dále? ano/ne ").lower()
